79e1f7c1a0ac95986fdc8ac980f16341da98f73d
Jiny <149757089@qq.com>
2024-05-24 11:01:29 +0800

    Feat: 登录页改用mui

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,118 +1,103 @@
 'use client'
 import { signIn, signOut } from 'next-auth/react'
-import { Form, Input, Dialog, Button } from 'antd-mobile'
 import { useState } from 'react'
 import { getSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
+import { TextField, Button, Typography, Stack } from '@mui/material'
+import { useForm, type SubmitHandler } from "react-hook-form"
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+
+type Inputs = {
+  email: string
+  password: string
+}
+
+const PasswordLoginForm = () => {
+  const router = useRouter()
+  const [isPassword, setIsPassword] = useState(true)
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    if(isPassword) {
+      const res = await signIn('password', {
+        email: data.email,
+        password: data.password,
+        redirect: false // 禁止重定向
+      })
+      if(res?.ok) {
+        router.push('/user')
+      } else {
+        alert('An error occurred, Please check your account and password')
+      }
+    } else {
+      // 注意ddos
+      const res = await signIn('email', { email: data.email, redirect: false })
+      if(res?.ok) {
+        alert('The login email has been sent, please check your email')
+      } else {
+        alert('An error occurred...')
+      }
+    }
+  }
+  // console.log(watch("email"))
+  return <form onSubmit={handleSubmit(onSubmit)}>
+    <Stack gap={2}>
+      <Typography component="h1" variant="h6">{ isPassword ? 'Password SignIn' : 'Email SignIn' }</Typography>
+      <TextField 
+        id="email" 
+        label="email" 
+        {...register('email', { required: 'email is required', pattern: { value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, message: 'not an email' } })}
+        error={!!errors.email}
+        helperText={errors.email?.message}
+        ></TextField>
+      { isPassword &&
+        <TextField 
+          id="password" 
+          label="password" 
+          type="password" 
+          {...register('password', { required: 'password is required' })}
+          error={!!errors.password}
+          helperText={errors.password?.message}
+          ></TextField>
+      }
+      <Button variant="contained" type="submit">SignIn</Button>
+      { isPassword ?
+        <>
+          <Typography color="#666">Don’t have an account?</Typography>
+          <Button variant="outlined" onClick={() => setIsPassword(false)}>switch to Email SignIn</Button>
+        </>
+        :
+        <>
+          <Typography color="#666">Already have an account?</Typography>
+          <Button variant="outlined" onClick={() => setIsPassword(true)}>switch to Password SignIn</Button>
+        </>
+      }
+    </Stack>
+  </form>
+}
 
 export default function Login() {
-  const router = useRouter()
-  const [isSignIn, setIsSignIn] = useState(true)
+  // todo: 已登录的进入登录页
   const [isLogin, setIsLogin] = useState(false)
+
   getSession().then(session => {
     if(session) {
       setIsLogin(true)
     }
   })
 
-  const onEmailSignIn = (values: any) => {
-    signIn('email', { email: values.email })
-  }
-
-  async function onPasswordSignIn(values: any) {
-    const res = await signIn('password', {
-      email: values.email,
-      password: values.password,
-      redirect: false // 禁止重定向
-    })
-    console.log(res)
-    if(res?.ok === false) {
-      alert('登录失败')
-    } else {
-      alert('登录成功')
-      router.push('/user')
-    }
-  }
-
-  function checkEmail(_: any, value: string) {
-    if(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value)) {
-      return Promise.resolve()
-    }
-    return Promise.reject(new Error('请输入正确的邮箱'))
-  }
-
   return (
-    <div className='h-screen bg-gradient-to-b from-[#2e026d] to-[#15162c] p-4'>
-      { isSignIn ? <Form
-        className='rounded-lg'
-        layout='horizontal'
-        onFinish={onPasswordSignIn}
-        mode='card'
-        footer={
-          <div>
-            <Button block type='submit' color='primary' size='large'>
-              SignIn
-            </Button>
-            <br />
-            <p className='text-white'>没有账号？</p>
-            <Button block color='default' size='large' onClick={() => setIsSignIn(false)}>
-              switch to Email SignIn
-            </Button>
-            <br />
-            { isLogin &&
-              <Button block color='warning' size='large' onClick={() => signOut()}>
-                SignOut
-              </Button>
-            }
-          </div>
-        }
-      >
-        <Form.Header><span className='text-white font-bold'>密码登录</span></Form.Header>
-        <Form.Item
-          name='email'
-          label='邮箱'
-          rules={[{ required: true, message: '邮箱不能为空' }, { validator: checkEmail }]}
-        >
-          <Input onChange={console.log} placeholder='请输入邮箱' />
-        </Form.Item>
-        <Form.Item
-          name='password'
-          label='密码'
-          rules={[{ required: true, message: '密码不能为空' }]}
-        >
-          <Input onChange={console.log} type="password" placeholder='请输入密码' />
-        </Form.Item>
-      </Form>
-      :
-      // 邮箱登陆
-      <Form
-        className='rounded-lg'
-        layout='horizontal'
-        onFinish={onEmailSignIn}
-        mode='card'
-        footer={
-          <div>
-            <Button block type='submit' color='primary' size='large'>
-              SignIn
-            </Button>
-            <br />
-            <p className='text-white'>已有账号并设置了密码？</p> 
-            <br />
-            <Button block color='default' size='large' onClick={() => setIsSignIn(true)}>
-              switch to password SignIn
-            </Button>
-          </div>
-        }
-      >
-        <Form.Header><span className='text-white font-bold'>邮箱登录</span></Form.Header>
-        <Form.Item
-          name='email'
-          label='邮箱'
-          rules={[{ required: true, message: '邮箱不能为空' }, { validator: checkEmail }]}
-        >
-          <Input onChange={console.log} placeholder='请输入邮箱' />
-        </Form.Item>
-      </Form> }
-    </div>
+    <Stack alignItems="center" py={2}>
+      <Card sx={{ width: 300, mt: 2 }}>
+        <CardContent>
+          <PasswordLoginForm></PasswordLoginForm>
+        </CardContent>
+      </Card>
+    </Stack>
   )
 }
